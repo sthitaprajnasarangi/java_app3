2b57fcb0e8de1a67892ed2a476bca60ed8524e9c
user1 <user1@gmail.com>
2023-08-23 14:18:08 +0530

    my java project souce code first commit

---
+++ Sum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Sum {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a Student name");
+        String name = sc.nextLine();
+        System.out.println(" You entered your name as " +name );
+        System.out.println("Enter Roll number");
+        String roll = sc.nextLine();
+        System.out.println(" You entered your roll number as " +roll);
+        System.out.println("Enter Class Room number");
+        String room = sc.nextLine();
+        System.out.println(" You entered your your class room number as " +room);
+        System.out.println("Enter phone number");
+        String phone = sc.nextLine();
+        System.out.println(" You entered your phone number as " +phone);
+        System.out.println("Enter email id");
+        String email = sc.nextLine();
+        System.out.println(" You entered your email as " +email);
+        System.out.println("Enter Address");
+        String address = sc.nextLine();
+        System.out.println(" You entered your address as " +address);
+    }
+}
